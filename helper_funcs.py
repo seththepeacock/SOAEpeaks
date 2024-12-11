fca872510d7d0595e641cfcd2922d647d7cfe757
seththepeacock <seththepeacock@gmail.com>
2024-12-10 20:46:28 -0500

    made simple transition

--- helper_funcs.py
+++ helper_funcs.py
@@ -60,6 +60,50 @@ def gen_samples(df, hwhm_max=100, prominence_max=25):
     X_x = (X_x - min_vals[:, None]) / (max_vals[:, None] - min_vals[:, None])
     # Return X (rows, N bins), y, and mins/maxes
     return X_x, y, mins_maxes, y_isolated_peaks 
+
+def gen_simple_samples(df, hwhm_max=100, prominence_max=20):
+    f_length = 8192
+    # Allocate memory for y (peak labels)
+    y = np.zeros((len(df), f_length))
+    # And for y_isolated_peaks
+    y_isolated_peaks = np.zeros((len(df), f_length))
+
+    f0_i_s = np.array(df['f0_i'], dtype=object)
+    hwhms = np.array(df['hwhm'], dtype=object)
+    prominences = np.array(df['prominence'], dtype=object)
+    # Rescale peaks to be in range ~[0, 1] (prominence could in principle be > 25 but this would be rare)
+    hwhms_scaled = hwhms / hwhm_max
+    prominences_scaled = prominences / prominence_max
+    # Grab frequency array
+    for row in range(len(df)):
+        # Get the peak list components for this row
+        f0_i_s_row = f0_i_s[row]
+        hwhms_scaled_row = hwhms_scaled[row]
+        hwhms_row = hwhms[row]
+        prominences_scaled_row = prominences_scaled[row]
+        if len(f0_i_s_row) != len(hwhms_scaled_row) or len(f0_i_s_row) != len(prominences_scaled_row):
+            raise ValueError("f0s, hwhms, and prominences must have the same length!")
+        # Add peak lists
+        for peak_num in range(len(f0_i_s_row)):
+            # Get peak components
+            f0_i = int(f0_i_s_row[peak_num])
+            # First add isolated position labels
+            y_isolated_peaks[row, f0_i] = 1
+            # Now we need to calculate how many bins +\- to label
+            label_hw = get_label_hw(hwhms_row[peak_num])
+            # min/max is to make sure we don't go out of bounds
+            f0_i_labels = np.arange(max(f0_i - label_hw, 0), min(f0_i + label_hw + 1, f_length - 1))
+            for bin_to_label in f0_i_labels:
+                # Add labels: [Yes/no peak, hwhm_scaled, prominence]
+                y[row, bin_to_label] = 1
+    X_x = np.stack(df['synth spectrum'])
+    min_vals = np.min(X_x, axis=1)  # Minimum along the rows
+    max_vals = np.max(X_x, axis=1)  # Maximum along the rows
+    mins_maxes = {"mins": min_vals, "maxes": max_vals}
+    # Rescale
+    X_x = (X_x - min_vals[:, None]) / (max_vals[:, None] - min_vals[:, None])
+    # Return X (rows, N bins), y, and mins/maxes
+    return X_x, y, mins_maxes, y_isolated_peaks 
     
 def estimate_noise_sigma(parent_spectrum, f=rfftfreq(32768, 1/44100), noise_domain='linear'):
     # First, we crop frequency axis and sample spectrum to 8-11kHz as these won't have any peaks and are just noise
@@ -265,6 +309,95 @@ def synthesize_spectrum(parent_spectrum, noise_floor, f=rfftfreq(32768, 1/44100)
         axs[0].scatter(f / 1000, synth_spectrum, label="Synthetic Spectrum", s=1)
         axs[0].plot(f / 1000, noise_floor, label="Noise Floor", color='orange')
         axs[0].set_title(f"Synthetic Spectrum: species={species}, {npeaks} peaks, {f0_dist} position distribution")
+        axs[0].set_xlabel("Frequency (kHz)")
+        axs[0].set_ylabel("dB SPL")
+        axs[0].set_ylim(global_min, global_max)  # Set common y-limits
+        peak_indices = peak_list[:, 0].astype(int)
+        axs[0].scatter(f[peak_indices] / 1000, synth_spectrum[peak_indices], color='red', label="True Peaks", s=1)
+        axs[0].legend()
+        # Plot the sample spectrum with noise floor on the second subplot
+        axs[1].plot(f / 1000, parent_spectrum, label="Sample Spectrum")
+        axs[1].plot(f / 1000, noise_floor, label="Noise Floor", color='orange')
+        axs[1].set_title(f"Parent Spectrum: {filepath if filepath else 'No filepath provided'}")
+        axs[1].set_xlabel("Frequency (kHz)")
+        axs[1].set_ylabel("dB SPL")
+        axs[1].legend()
+        axs[1].set_ylim(global_min, global_max)  # Set common y-limits
+        plt.tight_layout()
+
+        # Save the figure or show plot
+        if save_name:
+            plt.savefig(save_name)
+        else: 
+            # Show the plots
+            plt.show()
+
+    
+    return {'noise floor' : noise_floor, 'species' : species, 'synth spectrum' : synth_spectrum, 'f0_i' : peak_list[:, 0], 'hwhm' : peak_list[:, 1], 'prominence' : peak_list[:, 2]}
+
+def synthesize_simple_spectrum(parent_spectrum, noise_floor, f=rfftfreq(32768, 1/44100), species='General', filepath=None, plot=False, save_name=None, noise_domain='linear'):
+    # Get num bins
+    n_bins = len(noise_floor)
+    if n_bins != 8192:
+        raise ValueError(f"Expected noise floor to be 8192 bins, but it's {n_bins} bins!")
+    
+    npeaks = 1
+    spec_components = np.empty((npeaks + 1, n_bins))
+    
+    # Create a Generator instance
+    rng = np.random.default_rng()  # Default random generator instance
+    
+    # Add noise floor
+    spec_components[-1, :] = noise_floor
+    
+    # Create empty peak list
+    peak_list = np.empty((npeaks, 3))
+    # And a temporary one for spreading out peaks
+    f0s = [-1000]
+    
+    # Generate and add peaks
+    for i in range(npeaks):
+        # Peak Position (f0):
+        f0 = rng.uniform(f[0], f[-1])
+        
+        # Now we have an f0; lock this onto the grid and add to f0s
+        f0_i = np.argmin(np.abs(f - f0))
+        f0 = f[f0_i]
+        f0s.append(f0)
+        
+        # Width (HWHM): 
+        hwhm = rng.uniform(3, 100)
+
+        prominence = rng.uniform(1, 20)
+        
+        # Add peak
+        spec_components[i, :] = lorentzian(f, f0, hwhm, prominence)
+        
+        # Store peak info
+        peak_list[i, :] = [f0_i, hwhm, prominence]
+        
+        
+    # Combine everything into the final synthetic spectrum
+    synth_spectrum = np.sum(spec_components, axis=0)
+    
+    # Add gaussian noise
+    synth_spectrum  = estimate_and_add_noise(synth_spectrum, parent_spectrum, f=f, rng=rng, noise_domain=noise_domain)
+    
+    
+    if plot:
+
+        # Compute the global y-limits for both graphs
+        all_values = [synth_spectrum, noise_floor, parent_spectrum]
+        global_min = min([min(data) for data in all_values])
+        global_max = max([max(data) for data in all_values])
+
+        # Create the subplots
+        fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+
+        # Plot the synthetic spectrum on the first subplot
+        axs[0].scatter(f / 1000, synth_spectrum, label="Synthetic Spectrum", s=1)
+        axs[0].plot(f / 1000, noise_floor, label="Noise Floor", color='orange')
+        axs[0].set_title(f"Synthetic Spectrum: species={species}, {npeaks} peaks")
         axs[0].set_xlabel("Frequency (kHz)")
         axs[0].set_ylabel("dB SPL")
         axs[0].set_ylim(global_min, global_max)  # Set common y-limits
