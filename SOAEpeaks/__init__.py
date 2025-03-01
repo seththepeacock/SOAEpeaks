b23c9e09cd8efb1554cfbbdf37dcfc4b87f5b815
seththepeacock <seththepeacock@gmail.com>
2025-03-01 14:03:36 -0500

    turned into a package

---
+++ SOAEpeaks/__init__.py
@@ -0,0 +1,11 @@
+from . import funcs_df
+from .funcs_df import *
+
+from . import funcs_dsp
+from .funcs_dsp import *
+
+from . import funcs_synth
+from .funcs_synth import *
+
+from . import funcs_peakfit
+from .funcs_peakfit import *
